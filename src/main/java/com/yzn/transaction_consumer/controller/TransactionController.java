121ac4a35023e658081b3809792018311a5e8e6c
Yazan Alqadery <yazanalqadery@gmail.com>
2025-08-07 12:27:26 +0400

    Added a controller that exposes an endpoint for getting the transactions as a graph with nodes and relationships.

---
+++ src/main/java/com/yzn/transaction_consumer/controller/TransactionController.java
@@ -0,0 +1,40 @@
+package com.yzn.transaction_consumer.controller;
+
+
+import com.yzn.transaction_consumer.service.TransactionService;
+import org.neo4j.driver.Session;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+
+    @GetMapping("/fraud-cycles")
+    public ResponseEntity<?> getFraudCycles() {
+        try {
+            var response = transactionService.getFraudCycles();
+
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body("Error retrieving fraud cycles.");
+        }
+    }
+
+}
+
---
+++ src/main/java/com/yzn/transaction_consumer/service/TransactionService.java
@@ -0,0 +1,73 @@
+package com.yzn.transaction_consumer.service;
+
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+
+    private final Driver driver;
+
+    public TransactionService(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Map<String,Object> getFraudCycles(){
+        try(Session session = driver.session()){
+            var result = session.run("MATCH " +
+                    "(a1:Account)-[r1:SENT]->(t1:Transaction)-[r2:RECEIVED_BY]->(a2:Account), " +
+                    "(a2)-[r3:SENT]->(t2:Transaction)-[r4:RECEIVED_BY]->(a3:Account), " +
+                    "(a3)-[r5:SENT]->(t3:Transaction)-[r6:RECEIVED_BY]->(a1) " +
+                    "WHERE a1 <> a2 AND a2 <> a3 AND a1 <> a3 " +
+                    "RETURN a1, a2, a3, t1, t2, t3, r1, r2, r3, r4, r5, r6 " +
+                    "LIMIT 15");
+
+            Map<String, Map<String,Object>> nodes = new HashMap<>();
+            List<Map<String,Object>> relationships = new ArrayList<>();
+
+            while (result.hasNext()){
+                var record = result.next();
+
+                // Extracting nodes
+                List<String> nodeKeys = List.of("a1","a2","a3","t1","t2","t3");
+                for (String key : nodeKeys){
+                    var node = record.get(key).asNode();
+                    String nodeId = String.valueOf(node.id());
+                    nodes.putIfAbsent(nodeId,Map.of(
+                            "id",nodeId,
+                            "labels",node.labels(),
+                            "properies",node.asMap()
+                    ));
+                }
+
+                // Extracting relationships
+                List<String> relationKeys = List.of("r1","r2","r3","r4","r5","r6");
+                for (String key : relationKeys){
+                    var relation = record.get(key).asRelationship();
+                    String relationId = String.valueOf(relation.id());
+                    relationships.add(Map.of(
+                            "id",relationId,
+                            "type",relation.type(),
+                            "startNode",relation.startNodeId(),
+                            "endNode",relation.endNodeId(),
+                            "properties",relation.asMap()
+                    ));
+
+                }
+            }
+            Map<String ,Object> response = new HashMap<>();
+            response.put("nodes", nodes.values());
+            response.put("relationships", relationships);
+            return response;
+        }catch (Exception exception){
+            throw new RuntimeException("Error querying fraud cycles", exception);
+        }
+
+    }
+}
