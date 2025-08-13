3d1ba6502305bb31a39b0c8b1e3cf1a7708e3151
Yazan Alqadery <yazanalqadery@gmail.com>
2025-08-13 09:49:56 +0400

    Exposed endpoints for the motif services

--- src/main/java/com/yzn/transaction_consumer/controller/MotifController.java
+++ src/main/java/com/yzn/transaction_consumer/controller/MotifController.java
@@ -1,4 +1,45 @@
 package com.yzn.transaction_consumer.controller;
 
+
+import com.yzn.transaction_consumer.model.Motif;
+import com.yzn.transaction_consumer.service.MotifService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/motifs")
 public class MotifController {
+
+    private final MotifService motifService;
+
+    public MotifController(MotifService motifService) {
+        this.motifService = motifService;
+    }
+
+    @GetMapping("")
+    public List<Motif> getAllMotifs(){
+        return motifService.getAllMotifs();
+    }
+
+    @GetMapping("/{id}")
+    public Motif getMotifById(@PathVariable Integer id){
+        return motifService.getMotifById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMotif(@PathVariable Integer id){
+        motifService.deleteMotif(id);
+    }
+
+    @PostMapping("")
+    public Motif createMotif(@RequestBody Motif motif){
+        return motifService.createMotif(motif);
+    }
+
+    @PutMapping("")
+    public Motif updateMotif(@PathVariable Integer id, @RequestBody Motif motif){
+        return motifService.updateMotif(id,motif);
+    }
+
 }
